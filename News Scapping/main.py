8c50a9b849d149f9aa6fb829510da13e31b75ed8
Johnvimalasir001 <johnvimalasir@gmail.com>
2023-03-01 23:47:50 +0530

    updated

--- News Scapping/main.py
+++ News Scapping/main.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 
-
+details = []
 def news_scrape():
     url = requests.get('http://www.inshorts.com/en/read/technology').text
     soup = BeautifulSoup(url,'lxml')
@@ -11,13 +11,11 @@ def news_scrape():
         hl=news.find('a', class_='clickable').text
         time=news.find('span',class_='time').text
         date=news.find('span',class_='date').text
-        summary=news.find('div', class_='news-card-content news-right-box').text
+        summary = news.find(class_='news-card-content').find('div').text
         more_info=only.a['href']
         
-        with open(f'News/{index}.txt','w') as f:
-            
-            f.write(f'{time.strip()} on {date.strip()},\n')
-            f.write(f'Headline: {hl.strip()}\n')
-            f.write(f'Summary: {summary.strip()}\n')
-            f.write(f'More Info: {more_info.strip()}\n')
-        print(f'File saved: {index}')
+        detail=(f"Headline:{hl.strip()}\nSummary:{summary.strip()}\nMore:{more_info.strip()}").split("\n")
+        details.append(detail)
+    return details
+
+print(news_scrape())
