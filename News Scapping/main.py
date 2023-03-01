483fb26c6aa4d1b7219372a98e07a4e49a67dd98
Johnvimalasir001 <johnvimalasir@gmail.com>
2023-03-01 23:49:24 +0530

    updated

--- News Scapping/main.py
+++ News Scapping/main.py
@@ -6,15 +6,11 @@ def news_scrape():
     url = requests.get('http://www.inshorts.com/en/read/technology').text
     soup = BeautifulSoup(url,'lxml')
     newses=soup.find_all('div', class_='news-card z-depth-1')
-    only=soup.find('div',class_='news-card-author-time news-card-author-time-in-content')
-    for index,news in enumerate(newses):
+    for news in newses:
         hl=news.find('a', class_='clickable').text
-        time=news.find('span',class_='time').text
-        date=news.find('span',class_='date').text
         summary = news.find(class_='news-card-content').find('div').text
-        more_info=only.a['href']
         
-        detail=(f"Headline:{hl.strip()}\nSummary:{summary.strip()}\nMore:{more_info.strip()}").split("\n")
+        detail=(f"Headline:{hl.strip()}\nSummary:{summary.strip()}").split("\n")
         details.append(detail)
     return details
 
